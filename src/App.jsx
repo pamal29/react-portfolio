8d5d192a193b2c41995484d7da0c583b8455b3dd
pamal pahasara <pamalpahasara@gmail.com>
2025-11-03 22:02:09 +0530

    Initial commit: React portfolio with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4 rounded-lg">
+  Hello Tailwind v3!
+</div>
+    </>
+  )
+}
+
+export default App
