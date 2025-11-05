2380700606b46e47e127cebe2aa8570b1672d02a
pamal pahasara <pamalpahasara@gmail.com>
2025-11-06 02:05:59 +0530

    project button align

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ProjectCard({ title, description, image, link }) {
   return (
-    <div className="bg-black border border-white rounded-1xl overflow-hidden">
+    <div className="bg-black border border-white rounded-xl overflow-hidden flex flex-col h-full">
       <div className="w-full h-48 overflow-hidden">
         <img
           src={image}
@@ -11,19 +11,23 @@ export default function ProjectCard({ title, description, image, link }) {
         />
       </div>
 
-      <div className="p-5">
+      <div className="p-5 flex flex-col flex-grow">
         <h3 className="text-2xl font-semibold text-white mb-2">{title}</h3>
-        <p className="text-gray-400 mb-4">{description}</p>
+        <p className="text-gray-400 mb-4 flex-grow">{description}</p>
 
         {link && (
-          <button
-            onClick={() => window.open(link, "_blank")}
-            className="px-8 py-3 text-white rounded-full border border-white w-full"
-          >
-            View Project
-          </button>
+          <div className="relative group mt-auto">
+            <div className="absolute -inset-0.5 "></div>
+            <button
+              onClick={() => window.open(link, "_blank")}
+              className="relative px-8 py-3 bg-black text-white rounded-full border border-white w-full "
+            >
+              View Project
+            </button>
+          </div>
         )}
       </div>
+      
     </div>
   );
 }
