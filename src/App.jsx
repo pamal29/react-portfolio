febf39fb0c688ac9ad6b783c493d65f5d9d21e13
pamal pahasara <pamalpahasara@gmail.com>
2025-11-05 11:47:20 +0530

    hero section cutomized

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 
 // import './App.css'
-import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
+
 
 function App() {
 
 
   return (
     <>
-      {/* <Navbar /> */}
       <Hero />
+
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,23 +2,51 @@ import React from 'react';
 
 export default function Hero({ setActiveSection }) {
   return (
-    <section className="min-h-screen bg-black flex items-center justify-center m-0 p-0">
-      <div className="text-center">
-        <div className="mb-8">
-          <div className="w-32 h-32 mx-auto rounded-full bg-gradient-to-r from-purple-500 to-pink-500 shadow-[0_0_50px_rgba(168,85,247,0.5)] animate-pulse" />
+    <section className="min-h-screen bg-black flex items-center justify-center px-6 pt-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center max-w-6xl w-full">
+        
+        
+        <div className="text-center md:text-left">
+          <p className="text-xl text-white mb-8">
+            Hi I am 
+          </p>
+
+          <p className='text-5xl text-white mb-8'>
+            Pamal Pahasara
+          </p>
+          
+          <h2 className="text-7xl font-bold mb-8 text-white bg-clip-text">
+           Creative Developer 
+          </h2>
+          
+          
+          <div className="grid grid-cols-2 gap-4 justify-start max-w-md">
+            <button
+              onClick={() =>  window.open('https://github.com/pamal29', '_blank')}
+              className="px-8 py-3 text-white rounded-full border border-white w-full"
+            >
+              View Github
+            </button>
+            <button
+              onClick={() => setActiveSection && setActiveSection('projects')}
+              className="px-8 py-3 text-white rounded-full border border-white w-full"
+            >
+              Download CV
+            </button>
+          </div>
+
+          
         </div>
-        <h2 className="text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-purple-600">
-          Welcome
-        </h2>
-        <p className="text-xl text-gray-400 mb-8">
-          Creative Developer & Designer
-        </p>
-        <button 
-          onClick={() => setActiveSection('projects')}
-          className="px-8 py-3 bg-purple-600 hover:bg-purple-700 rounded-full shadow-[0_0_30px_rgba(168,85,247,0.5)] hover:shadow-[0_0_50px_rgba(168,85,247,0.8)] transition-all"
-        >
-          View My Work
-        </button>
+
+  
+        <div className="flex justify-center md:justify-end">
+          <img src='../../public/hero-picture.jpg' 
+               alter='picture'
+               className="w-64 h-64 rounded-full object-cover shadow-[0_0_50px_rgba(255,255,255,0.8)]">
+
+          </img>
+    
+        </div>
       </div>
     </section>
   );
