f73d029008372fcda793e97a4476088a076d0783
pamal pahasara <pamalpahasara@gmail.com>
2025-11-26 10:38:09 +0530

    add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgDark: "#0E0E15",
+        cardDark: "#1A1A24",
+        textLight: "#E5E5E7",
+        accentOrange: "#F97316",
+        accentGreen: "#4ADE80",
+        borderDark: "#2A2A36",
+      }
+    },
   },
   plugins: [],
-};
+}
