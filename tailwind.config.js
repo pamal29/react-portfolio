60598240ec95c21f47826bf3193c0d8299160152
pamal pahasara <pamalpahasara@gmail.com>
2025-11-27 18:42:46 +0530

    added neonPurple

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        bgDark: "#0E0E15",
-        cardDark: "#1A1A24",
-        textLight: "#E5E5E7",
-        accentOrange: "#F97316",
-        accentGreen: "#4ADE80",
-        borderDark: "#2A2A36",
-      }
+        neonPurple: "#B000FF",
+  },
     },
   },
   plugins: [],
