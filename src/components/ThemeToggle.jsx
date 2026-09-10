de7f9f43e8f0e1d5fe54ee8d52233572ceac2b58
pamal pahasara <pamalpahasara@gmail.com>
2026-09-10 22:04:29 +0530

    fix import issue

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,5 @@
 import { Sun, Moon } from "lucide-react";
-import { useTheme } from "../context/ThemeProvider";
+import { useTheme } from "../context/ThemeContext";
 
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
